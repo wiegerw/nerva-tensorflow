fbe0cf2de9f5a647b83c4ee6e17e847d77981d19
“Wieger <j.w.wesselink@tue.nl>
2025-09-02 14:26:28 +0200

    Add installation of tensorflow to the Sphinx workflow
    - using mocked Tensorflow is too hard to get right

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -4,7 +4,7 @@ import sys
 from datetime import datetime
 
 # Add src to path so autodoc can import the package
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 project = 'nerva_tensorflow'
 author = 'Wieger Wesselink and contributors'
@@ -18,49 +18,28 @@ extensions = [
     'sphinx.ext.autosummary',
 ]
 
-# -----------------------------
-# Mock heavy or unavailable imports (avoid installing TensorFlow on CI)
-# -----------------------------
+# Avoid heavy imports during autodoc builds on CI
 autodoc_mock_imports = [
     'tensorflow',
     'sklearn',
 ]
 
-# Move type hints into description to avoid evaluation errors on mocked objects
-autodoc_typehints = "description"
+# Generate autosummary pages automatically
+autosummary_generate = True
 
-# -----------------------------
-# Autosummary
-# -----------------------------
-autosummary_generate = True  # generate _autosummary files automatically
-
-# -----------------------------
 # Napoleon settings for Google/NumPy style docstrings
-# -----------------------------
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_use_param = True
 napoleon_use_rtype = True
 
-# -----------------------------
-# HTML output
-# -----------------------------
 html_theme = 'sphinx_rtd_theme'
 
-# -----------------------------
-# Autodoc ordering and defaults
-# -----------------------------
+# Keep output simple and fast to build
 autodoc_member_order = 'bysource'
 autodoc_default_options = {
     'members': True,
     'undoc-members': False,
     'show-inheritance': True,
 }
-
-# -----------------------------
-# Optional: Speed up builds
-# -----------------------------
-# If desired, skip inherited members from mocked base classes
-autodoc_inherit_docstrings = False
-
